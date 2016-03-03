792bbcc4fa2f2ea8ab95a917a619da72cbf0bec2
DElboy <delpeter@gmail.com>
2016-03-03 22:47:01 +0000

    Cron Details

--- Week4/cron.sh
+++ Week4/cron.sh
@@ -21,8 +21,18 @@ mv week4backup.tar.gz $HOME/backups
 #esc
 #:wq
 
+#min (0-59)
+#hr (0-23)
+#dayofmonth(1-31)
+#month(1-12)
+#dayofweek(0-6) - 0 to 6 are Sunday to Sathurday, or use names: 7 is Sunday, that same as 0
 
-
+#@yearly 	Run once a year at midnight of January 1 	0 0 1 1 *
+#@monthly 	Run 1ce a mth @ midnight of the 1st day of the mth 0 0 1 * *
+#@weekly 	Run once a week at midnight on Sunday morning 	0 0 * * 0
+#@daily 	Run once a day at midnight 	0 0 * * *
+#@hourly 	Run once an hour at the beginning of the hour 	0 * * * *
+#@reboot 	Run at startup 	@reboot
 
 
 
